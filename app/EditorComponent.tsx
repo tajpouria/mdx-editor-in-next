f5d2c7fecf413918442c4c11e975dac4f15b1ce0
Petyo Ivanov <underlog@gmail.com>
2024-04-15 14:55:33 +0300

    Update to latest next

--- app/EditorComponent.tsx
+++ app/EditorComponent.tsx
@@ -1,19 +1,26 @@
-'use client'
+"use client";
 
-import { MDXEditor, MDXEditorMethods, headingsPlugin } from "@mdxeditor/editor"
-import {FC} from 'react'
+import { MDXEditor, MDXEditorMethods, headingsPlugin } from "@mdxeditor/editor";
+import { FC } from "react";
 
 interface EditorProps {
-  markdown: string
-  editorRef?: React.MutableRefObject<MDXEditorMethods | null>
+  markdown: string;
+  editorRef?: React.MutableRefObject<MDXEditorMethods | null>;
 }
 
 /**
  * Extend this Component further with the necessary plugins or props you need.
- * proxying the ref is necessary. Next.js dynamically imported components don't support refs. 
-*/
+ * proxying the ref is necessary. Next.js dynamically imported components don't support refs.
+ */
 const Editor: FC<EditorProps> = ({ markdown, editorRef }) => {
-  return <MDXEditor ref={editorRef} markdown={markdown} plugins={[headingsPlugin()]} />
-}
+  return (
+    <MDXEditor
+      onChange={(e) => console.log(e)}
+      ref={editorRef}
+      markdown={markdown}
+      plugins={[headingsPlugin()]}
+    />
+  );
+};
 
-export default Editor
+export default Editor;
